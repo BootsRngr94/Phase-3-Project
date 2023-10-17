658afd5c3450e1ddef621c9ced500cf84c0f2354
shawnconway <shawnway210@gmail.com>
2023-10-17 10:50:14 -0400

    model update

--- lib/models/Game.py
+++ lib/models/Game.py
@@ -1,8 +1,32 @@
 class Game:
+
+    all = []
     def __init__(self, title, genre, platform):
         self.title = title
         self.genre = genre
         self.platform = platform
 
-        genre.add_game(self)
-        platform.add_game(self)
+        Game.all.append(self)
+    
+        self.genre._games.append(self)
+        self.genre._platforms.append(self.platform)
+
+        self.platform._games.append(self)
+        self.platform._genres.append(self.genre)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if type(title) == str and 0 < len(title) and not hasattr(self,"title"):
+            self._title = title
+        else:
+            raise Exception("The title must be a string and have more than 0 characters")
+        
+   
+
+   
+    
+        
--- lib/models/Genre.py
+++ lib/models/Genre.py
@@ -1,7 +1,26 @@
-class Genre: 
+class Genre:
+    all = [] 
     def __init__(self, name):
-        self.name = name 
-        self.games = []
+        self.name = name
+    
+        self._platforms = []
+        self._games = []
         
-    def add_game(self, game):
-        self.games.append(game)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if type(name) == str and 0 < len(name) and hasattr(self,"name"):
+            self._name = name
+        else:
+            raise Exception("The name must be a string and have more than 0 characters")
+
+        
+    def add_game(self):
+        return self._games
+    
+    def add_platform(self):
+        return self._platforms
